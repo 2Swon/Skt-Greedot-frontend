fb2a8988aff3ffbe0ea9d0e374f94400afffc26e
LeeSeongWon <tjddnjs10617@naver.com>
2024-02-22 17:56:28 +0900

    error fix

--- lib/service/gree_service.dart
+++ lib/service/gree_service.dart
@@ -91,8 +91,7 @@ class ApiServiceGree {
     );
 
     if (response.statusCode == 200) {
-      String decodedResponse = utf8.decode(response.bodyBytes);
-      var jsonResponse = json.decode(decodedResponse);
+      List jsonResponse = json.decode(response.body);
       return jsonResponse.map((data) => Gree.fromJson(data)).toList();
     } else {
       throw Exception('Failed to load grees.');
@@ -106,15 +105,16 @@ class ApiServiceGree {
     if (token == null) {
       throw Exception('no token');
     }
+    print('test');
     var response = await http.get(
       Uri.parse(url),
       headers: {
         "Authorization": "Bearer $token",
       },
     );
+    print(response.statusCode);
     if (response.statusCode == 200) {
-      String decodedResponse = utf8.decode(response.bodyBytes);
-      return json.decode(decodedResponse);
+      return json.decode(response.body);
     } else {
       throw Exception('Failed to load gree.');
     }
